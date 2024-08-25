d3cfbcbd9c6528cc03448f5c64971a465308f66f
DavidAGullo <davidg@fenex.studio>
2024-08-24 23:42:31 -0500

    Updating the .gitignore file to ignore the .vscode folder and uploading progress on the script.

--- adminbyrequest.py
+++ adminbyrequest.py
@@ -1,13 +1,22 @@
-from dotenv import load_dotenv
 import requests
 
 class AdminByRequest:
     def __init__(self, apikey):
-        load_dotenv()
         self.api_key = apikey
-        self.headers
-        self.url = 'https://dc1api.adminbyrequest.com/'
+        self.url = 'https://dc2api.adminbyrequest.com/'
         
-# /auditlog - Get the audit log API stuff
-    def auditlog():
-        pass
+# /auditlog - Audit Log API extension
+
+    def get_all_audit_logs(self):
+        audit_logs : str = []
+        count = 0
+        url = self.url + 'auditlog'
+        headers = {
+            "apikey": self.api_key
+        }
+        response = requests.get(url, headers=headers)
+        for log in response.json():
+            audit_logs[log] = "Entry: " + str(count) + " | User: " + log['user']['account'] + log['elevatedApplications']['name']
+            count+=1
+        return audit_logs
+        
