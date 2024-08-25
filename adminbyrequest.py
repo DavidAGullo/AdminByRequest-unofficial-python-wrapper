e877ae8d10bb2af0bd73aa9aaf55a6fa1f7091fa
David Gullo <davidg@fenex.studio>
2024-08-25 00:43:28 -0500

    Fixed error in requests methods, will now return JSON data.

--- adminbyrequest.py
+++ adminbyrequest.py
@@ -7,16 +7,53 @@ class AdminByRequest:
         
 # /auditlog - Audit Log API extension
 
+    # Get all audit logs as JSON
     def get_all_audit_logs(self):
-        audit_logs : str = []
-        count = 0
+        audit_logs = []
         url = self.url + 'auditlog'
         headers = {
             "apikey": self.api_key
         }
         response = requests.get(url, headers=headers)
-        for log in response.json():
-            audit_logs[log] = "Entry: " + str(count) + " | User: " + log['user']['account'] + log['elevatedApplications']['name']
-            count+=1
         return audit_logs
-        
+    
+    # Get audit log by ID as JSON
+    def get_audit_log_by_id(self, id):
+        audit_log = []
+        url = self.url + 'auditlog/' + str(id)
+        headers = {
+            "apikey": self.api_key
+        }
+        response = requests.get(url, headers=headers)
+        return audit_log
+    
+    # Get audit log by computer as JSON
+    def get_audit_log_by_computer(self, computername):
+        audit_logs = []
+        url = self.url + 'computers/' + computername + "/auditlog"
+        headers = {
+            "apikey": self.api_key
+        }
+        response = requests.get(url, headers=headers)
+        return audit_logs
+    
+    # Get audit log by user as JSON
+    def get_audit_log_by_user(self, username):
+        audit_logs = []
+        url = self.url + 'users/' + username + "/auditlog"
+        headers = {
+            "apikey": self.api_key
+        }
+        response = requests.get(url, headers=headers)
+        return audit_logs
+    
+    # Get the timeNow Value which will be used to calculate until the next delta [This number increases by second]
+    def get_delta_audit_logs(self):
+        audit_logs = []
+        url = self.url + 'auditlog/delta'
+        headers = {
+            "apikey": self.api_key
+        }
+        response = requests.get(url, headers=headers)
+        return audit_logs["timeNow"]
+    
